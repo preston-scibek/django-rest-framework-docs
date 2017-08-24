db955e914a2b94d73b2120187d685c3b2fc096ab
Preston <psscibek@gmail.com>
2017-08-24 17:16:35 -0400

    updated liveapi

--- rest_framework_docs/static/rest_framework_docs/js/components/view_request.js
+++ rest_framework_docs/static/rest_framework_docs/js/components/view_request.js
@@ -9,9 +9,24 @@ var ViewRequest = React.createClass({
       payload: this.props.payload,
     };
   },
+    
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({
+      payload: nextProps.payload
+    });
+  },
 
 
   render: function () {
+    if (!this.state.payload) {
+      return (
+        <div>
+          <h3>Response</h3>
+          <p className='lead text-center'>Awaiting request...</p>
+        </div>
+      );
+    }
+
 
     var requestJSON = JSONpp.prettyPrint(this.state.payload);
     return (
